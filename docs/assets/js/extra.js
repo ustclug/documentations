555ebcf737f53f9da7f33ca4ee420223e2f545dc
iBug <git@ibugone.com>
2020-05-21 02:02:34 +0800

    JS: Expand 2nd-level navigation by default
    In the near future we're going to enable tabs
    This is a preparation for that

--- docs/assets/js/extra.js
+++ docs/assets/js/extra.js
@@ -6,22 +6,21 @@ function load_navpane() {
   var width = window.innerWidth;
   if (width <= 1200)
     return;
+  const EXPAND_LEVELS = [1, 2];
 
   var nav = document.getElementsByClassName("md-nav");
-  for(var i = 0; i < nav.length; i++) {
+  for (var i = 0; i < nav.length; i++) {
     if (typeof nav.item(i).style === "undefined")
       continue;
 
-    if (nav.item(i).getAttribute("data-md-level") === "1" && nav.item(i).getAttribute("data-md-component")) {
+    if (EXPAND_LEVELS.includes(nav.item(i).getAttribute("data-md-level")) && nav.item(i).getAttribute("data-md-component")) {
       nav.item(i).style.display = 'block';
       nav.item(i).style.overflow = 'visible';
     }
   }
 
   var nav = document.getElementsByClassName("md-nav__toggle");
-  for (var i = 0; i < nav.length; i++) {
-    if ((nav.item(i).id.match(/-/g) || []).length > 1)
-      continue;
-    nav.item(i).checked = true;
-  }
+  for (var i = 0; i < nav.length; i++)
+    if (EXPAND_LEVELS.includes((nav.item(i).id.match(/-/g) || []).length))
+      nav.item(i).checked = true;
 }
