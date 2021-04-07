e446cb56a6aa69aa4427bb826a39d24be0817bd6
iBug <git@ibugone.com>
2021-04-07 13:17:26 +0800

    Fix navigation expander script

--- docs/assets/js/extra.js
+++ docs/assets/js/extra.js
@@ -8,19 +8,21 @@ function load_navpane() {
     return;
   const EXPAND_LEVELS = [1, 2];
 
+  /* For MkDocs Material <= 5.x (maybe 6.x?)
   var nav = document.getElementsByClassName("md-nav");
   for (var i = 0; i < nav.length; i++) {
     if (typeof nav.item(i).style === "undefined")
       continue;
 
-    if (EXPAND_LEVELS.includes(nav.item(i).getAttribute("data-md-level")) && nav.item(i).getAttribute("data-md-component")) {
+    if (EXPAND_LEVELS.includes(nav.item(i).getAttribute("data-md-level"))) {
       nav.item(i).style.display = 'block';
       nav.item(i).style.overflow = 'visible';
     }
   }
+  */
 
   var nav = document.getElementsByClassName("md-nav__toggle");
   for (var i = 0; i < nav.length; i++)
-    if (EXPAND_LEVELS.includes((nav.item(i).id.match(/_\\d+/g) || []).length))
+    if (EXPAND_LEVELS.includes((nav.item(i).id.match(/_\d+/g) || []).length))
       nav.item(i).checked = true;
 }
