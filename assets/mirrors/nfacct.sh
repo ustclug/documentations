eacc8a9c585a01361194352c8810e99b79f06808
GitHub <noreply@github.com>
2023-12-31 07:02:02 +0000

    Auto deploy from GitHub Actions build 379
    [0f10598] iBug: Revert "mirrors/monitor: Change script to output integers"

--- assets/mirrors/nfacct.sh
+++ assets/mirrors/nfacct.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo nfacct list | awk '-F[ ,;]' "{printf(\"nfacct,object=%s bytes=%ii,pkgs=%ii\n\",\$11,\$8,\$4)}"
+sudo nfacct list | awk '-F[ ,;]' "{printf(\"nfacct,object=%s bytes=%i,pkgs=%i\n\",\$11,\$8,\$4)}"
