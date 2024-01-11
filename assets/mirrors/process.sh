26da39dbfb4fb4548369c9ac1964dba4b552b4d3
GitHub <noreply@github.com>
2024-01-11 12:41:54 +0000

    Auto deploy from GitHub Actions build 385
    [855a2db] iBug: telegraf process.sh: Ignore state=I

--- assets/mirrors/process.sh
+++ assets/mirrors/process.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ps -e -o s= -o comm= |
-  grep -v '^S ' |
+  grep -v '^[SI] ' |
   sed 's|/.*$|/|g' |
   sort | uniq -c |
   awk '{printf("process,state=%s,name=%s count=%ii\n",$2,$3,$1)}'
