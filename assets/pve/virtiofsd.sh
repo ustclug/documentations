0dc654d3ad148f08821013f6a06c09321a2342a3
GitHub <noreply@github.com>
2024-12-22 20:33:44 +0000

    Auto deploy from GitHub Actions build 491
    [0e3bf10] iBug: Update virtiofsd.sh for docker2

--- assets/pve/virtiofsd.sh
+++ assets/pve/virtiofsd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then
+if test $# -ne 2; then
   echo "Need exactly 2 arguments" >&2
   exit 1
 fi
@@ -8,14 +8,18 @@ fi
 VMID="$1"
 PHASE="$2"
 
-[ "$VMID" -eq 230 ] || exit 0
+case "$VMID" in
+  201) SHARED_DIR=/mnt/docker2 ;;
+  230) SHARED_DIR=/mnt/mirrorlog ;;
+  *) exit 0 ;;
+esac
 
-NAME=virtiofsd-230
+NAME="virtiofsd-$VMID"
 SOCKPATH="/run/$NAME.sock"
 
 case "$PHASE" in
   pre-start)
-    systemctl stop "$NAME".service
+    systemctl stop "$NAME".service || true
     rm -f "$SOCKPATH" "$SOCKPATH".pid
 
     systemd-run \
@@ -24,7 +28,7 @@ case "$PHASE" in
       /usr/libexec/virtiofsd \
       --syslog \
       --socket-path "$SOCKPATH" \
-      --shared-dir /mnt/mirrorlog \
+      --shared-dir "$SHARED_DIR" \
       --announce-submounts \
       --inode-file-handles=mandatory
       ;;
